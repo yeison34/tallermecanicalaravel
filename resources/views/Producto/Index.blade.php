efe84616c6f6dd1ae46908de4884178f6db3857c
yeison34 <jeisonperenguez1998@gmail.com>
2023-05-28 00:02:16 -0500

    menu ventas

--- resources/views/Producto/Index.blade.php
+++ resources/views/Producto/Index.blade.php
@@ -24,6 +24,7 @@
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="{{route('Index')}}">Inicio</a></li>
                         <li class="nav-item"><a class="nav-link" href="{{route('ListadoProductos')}}">Lista Productos</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{route('ventas')}}">Ventas</a></li>
                     </ul>
                 </div>
             </div>
